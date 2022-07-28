4179ae33f5543e60d87496423d78a8c8aef32502
William Gadney <gadnex@gmail.com>
2022-07-28 15:35:22 +0200

    Update to spring boot 2.7.2 and addc custom metrics to SleepService

--- notification-service/src/main/java/net/binarypaper/notificationservice/Application.java
+++ notification-service/src/main/java/net/binarypaper/notificationservice/Application.java
@@ -2,7 +2,11 @@ package net.binarypaper.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableKafka
@@ -10,6 +14,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	// Required for @Timed annotation in SleepService to record the metric
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 
 }
--- notification-service/src/main/java/net/binarypaper/notificationservice/notification/SleepService.java
+++ notification-service/src/main/java/net/binarypaper/notificationservice/notification/SleepService.java
@@ -4,11 +4,27 @@ import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.stereotype.Service;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Service
 public class SleepService {
-    
+
+    private final Counter sleepCounter;
+
+    public SleepService(MeterRegistry meterRegistry) {
+        sleepCounter = Counter.builder("sleep.counter")
+                .tag("mytag", "123")
+                .description("A counter of how many times the sleep method was called")
+                .register(meterRegistry);
+
+    }
+
     @NewSpan
+    @Timed(value = "sleep.method", extraTags = {"mytag", "456"})
     public void sleep(@SpanTag("milliSeconds") long milliSeconds) throws InterruptedException {
+        sleepCounter.increment();
         Thread.sleep(milliSeconds);
     }
 }
